4fa9ab45bd22a597ed9342533aaf044a413b72e6
Krzysiek <krzysio.apps@gmail.com>
2026-05-04 13:26:32 +0200

    fix2

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const ua = req.headers.get("user-agent") || "";
-
-  if (
-    ua.includes("facebookexternalhit") ||
-    ua.includes("Facebot")
-  ) {
-    return NextResponse.next();
-  }
-
+export function middleware() {
   return NextResponse.next();
 }
