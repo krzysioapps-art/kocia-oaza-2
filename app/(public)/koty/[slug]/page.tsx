b8afaa3c72da2e760094e5d83f3c9369010b7b95
Krzysiek <krzysio.apps@gmail.com>
2026-05-04 15:16:44 +0200

    fixxxx

--- app/(public)/koty/[slug]/page.tsx
+++ app/(public)/koty/[slug]/page.tsx
@@ -1,15 +1,6 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-// ważne dla botów (Next 15/16)
-export const metadata = {
-    // jeśli używasz generateMetadata – zostaw, ale:
-    other: {
-        // hint dla Next, żeby nie streamował head dla botów
-        'x-robots-render': 'full',
-    },
-};
-
 import PostList from "@/app/components/news/PostList";
 import "@/app/style/koty/cat-page.css";
 import CatTabs from "@/app/components/ui/CatTabs";
