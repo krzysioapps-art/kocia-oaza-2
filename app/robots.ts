96aed6bec4439829a15807326c90eeac1f7555f4
Krzysiek <krzysio.apps@gmail.com>
2026-05-04 13:07:48 +0200

    fix robots for facebook

---
+++ app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+    ],
+  };
+}
